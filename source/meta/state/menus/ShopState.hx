414d068ba77c87cfc67e015f5fdbca16a0b1cb4c
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-12-04 19:04:00 -0500

    Update ShopState.hx

--- source/meta/state/menus/ShopState.hx
+++ source/meta/state/menus/ShopState.hx
@@ -168,7 +168,7 @@ class ShopState extends MusicBeatState
 	var confirmingPurchase:Bool = false;
 	var inCutscene:Bool = false;
 
-	static var page:Int = SHOP;
+	static var page:meta.state.menus.SubMenu = SHOP;
 
 	var gameBoy:FlxAnimate;
 	var blackOverlay:FlxSprite;
