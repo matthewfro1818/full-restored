3d0cbf98aed5fe09514bc5c7ee4c7035a0a20733
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-10-30 04:24:08 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -53,44 +53,12 @@ set /p menu2="Would you like to automatically make the APIStuff file? [Y/N]"
 :APIStuffYes
 rem Stores the APIStuff.hx contents automatically
 cd source
-(
 echo package;
 echo class APIStuff
 echo {
 echo         public static var API:String = "";
 echo         public static var EncKey:String = "";
 echo }
-)>APIStuff.hx
+>APIStuff.hx
 cd ..
 goto APIStuffNo
-
-
-:APIStuffNo
-cls
-title FNF Setup - User action required
-set /p menu="Would you like to install Visual Studio Community and components? (Necessary to compile/ 5.5GB) [Y/N]"
-       if %menu%==Y goto InstallVSCommunity
-       if %menu%==y goto InstallVSCommunity
-       if %menu%==N goto SkipVSCommunity
-       if %menu%==n goto SkipVSCommunity
-       cls
-
-
-:SkipVSCommunity
-cls
-title FNF Setup - Success
-echo Setup successful. Press any key to exit.
-pause >nul
-exit
-
-:FixTransitionBug
-title FNF Setup - Installing libraries
-haxelib git flixel-addons https://github.com/HaxeFlixel/flixel-addons
-goto UserActions2
-
-:InstallVSCommunity
-title FNF Setup - Installing Visual Studio Community
-curl -# -O https://download.visualstudio.microsoft.com/download/pr/3105fcfe-e771-41d6-9a1c-fc971e7d03a7/8eb13958dc429a6e6f7e0d6704d43a55f18d02a253608351b6bf6723ffdaf24e/vs_Community.exe
-vs_Community.exe --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows10SDK.19041 -p
-del vs_Community.exe
-goto SkipVSCommunity
